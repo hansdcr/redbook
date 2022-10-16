2b69eac77576f1b24f5dd8bcfa219acd31c528cf
464501551@qq.com <464501551@qq.com>
2022-10-16 21:45:38 +0800

    创建用户，写入数据库

--- book-api/src/main/java/org/example/Application.java
+++ book-api/src/main/java/org/example/Application.java
@@ -2,12 +2,15 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "org.example.mapper")
+// @ComponentScan(basePackages = {"org.example", "org.n3r.idworker"}) // 如果有包扫描不到，可以在这里加入
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
--- book-service/src/main/java/org/example/service/impl/UserServiceImpl.java
+++ book-service/src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -1,14 +1,26 @@
 package org.example.service.impl;
 
 import org.apache.catalina.User;
+import org.example.enums.Sex;
+import org.example.enums.YesOrNo;
 import org.example.mapper.UsersMapper;
 import org.example.pojo.Users;
 import org.example.service.UserService;
+import org.example.utils.DateUtil;
+import org.example.utils.DesensitizationUtil;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
+
+import java.util.Date;
 
 public class UserServiceImpl implements UserService {
   @Autowired private UsersMapper usersMapper;
+  @Autowired private Sid sid;
+  // 写死的一个用户默认头像
+  private static final String USER_FACE1 =
+      "http://122.152.205.72:88/group1/M00/00/05/CpoxxF6ZUySASMbOAABBAXhjY0Y649.png";
 
   @Override
   public Users queryMobileIsExist(String mobile) {
@@ -19,8 +31,32 @@ public class UserServiceImpl implements UserService {
     return users;
   }
 
+  @Transactional
   @Override
   public Users createUser(String mobile) {
-    return null;
+    // 获得全局唯一的ID
+    String userId = sid.nextShort();
+    Users user = new Users();
+    user.setId(userId);
+    user.setMobile(mobile);
+    user.setNickname("用户：" + DesensitizationUtil.commonDisplay(mobile));
+    user.setImoocNum("用户：" + DesensitizationUtil.commonDisplay(mobile));
+    user.setFace(USER_FACE1);
+
+    user.setBirthday(DateUtil.stringToDate("1900-01-01"));
+    user.setSex(Sex.secret.type);
+
+    user.setCountry("中国");
+    user.setProvince("");
+    user.setCity("");
+    user.setDistrict("");
+    user.setDescription("这家伙很懒，什么都没留下~");
+    user.setCanImoocNumBeUpdated(YesOrNo.YES.type);
+
+    user.setCreatedTime(new Date());
+    user.setUpdatedTime(new Date());
+
+    usersMapper.insert(user);
+    return user;
   }
 }
