3a3ffab118291807fb9e9318476d11a60959159f
464501551@qq.com <464501551@qq.com>
2022-10-23 15:59:07 +0800

    kafka 生产者-异步回调发送

--- kafkademo01/src/main/java/org/example/kafkademo01/producer/ProducerSample.java
+++ kafkademo01/src/main/java/org/example/kafkademo01/producer/ProducerSample.java
@@ -11,8 +11,9 @@ public class ProducerSample {
   public static final String KAFKA_HOST = "127.0.0.1:9092";
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    // producerSend();
-    producerSyncSend();
+    //    producerSend();
+    //    producerSyncSend();
+    producerSendWithCallback();
   }
 
   /*
@@ -81,10 +82,55 @@ public class ProducerSample {
       RecordMetadata recordMetadata = send.get();
       System.out.println(
           key
-              + "partition : "
+              + " , partition : "
               + recordMetadata.partition()
               + " , offset : "
               + recordMetadata.offset());
+    }
+
+    // 所有的通道打开都需要关闭
+    producer.close();
+  }
+
+  /*
+     Producer异步发送带回调函数
+  */
+  public static void producerSendWithCallback() {
+    Properties properties = new Properties();
+    properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_HOST);
+    properties.put(ProducerConfig.ACKS_CONFIG, "all");
+    properties.put(ProducerConfig.RETRIES_CONFIG, "0");
+    properties.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+    properties.put(ProducerConfig.LINGER_MS_CONFIG, "1");
+    properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+
+    properties.put(
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+    properties.put(
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+
+    // Producer的主对象
+    Producer<String, String> producer = new KafkaProducer<>(properties);
+
+    // 消息对象 - ProducerRecoder
+    for (int i = 0; i < 10; i++) {
+      ProducerRecord<String, String> record =
+          new ProducerRecord<>(TOPIC_NAME, "key-" + i, "value-" + i);
+
+      producer.send(
+          record,
+          new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+              System.out.println(
+                  "partition : "
+                      + recordMetadata.partition()
+                      + " , offset : "
+                      + recordMetadata.offset());
+            }
+          });
     }
 
     // 所有的通道打开都需要关闭
